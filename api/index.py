30e95ff565e27343af3ef39b15df1767039b9225
tugutam55-hue <tugutam55@gmail.com>
2026-02-27 16:31:23 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
@@ -11,9 +12,14 @@ app = FastAPI()
 app.include_router(scan_router)
 app.include_router(config_router)
 
-app.mount("/static", StaticFiles(directory="web/static"), name="static")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-templates = Jinja2Templates(directory="web/templates")
+TEMPLATE_DIR = os.path.join(BASE_DIR, "..", "web", "templates")
+STATIC_DIR = os.path.join(BASE_DIR, "..", "web", "static")
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+templates = Jinja2Templates(directory=TEMPLATE_DIR)
 
 
 @app.get("/", response_class=HTMLResponse)
@@ -22,4 +28,4 @@ def home(request: Request):
     return templates.TemplateResponse(
         "home.html",
         {"request": request}
-  )
+    )
